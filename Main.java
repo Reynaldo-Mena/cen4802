1648dc8496a4f9826e9b14f33be1cce5f6ea5e5c
Reynaldo-Mena <134249465+Reynaldo-Mena@users.noreply.github.com>
2024-08-25 21:11:31 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,6 +5,13 @@ public class Main {
         int result = fibonacci(n);
         System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
     }
+
+    /**
+     * Gets the nth number in the fibonacci sequence using recursive method.
+     *
+     * @param n position in the fibonacci sequence
+     * @return the nth fibonacci number
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
